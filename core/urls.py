35ed0737f427d03143b465b9171d9df7ee2a512e
Matin <81927498+matin2002programmer@users.noreply.github.com>
2023-07-04 23:00:41 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('https://www.instagram.com/matindevilish_boy/')),
     path('admin/', admin.site.urls),
     path('api/auth/', include("accountModule.urls", namespace='accountModule')),
     path('products/', include("product_module.urls")),
