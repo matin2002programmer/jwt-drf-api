4fe1b7802d46ae3ac3e681b5f56d3cb7e67a14d5
Matin <81927498+matin2002programmer@users.noreply.github.com>
2023-07-04 22:39:22 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include("accountModule.urls", namespace='accountModule')),
     path('products/', include("product_module.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
