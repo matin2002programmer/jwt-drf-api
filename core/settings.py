a69916b32e741903366f31054713e527cb0b351e
Matin <81927498+matin2002programmer@users.noreply.github.com>
2023-07-04 22:20:14 +0330

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -115,8 +115,12 @@ AUTH_USER_MODEL = "accountModule.User"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'NCNMjdeHFdqgUj3g6i75',
+        'HOST': 'containers-us-west-143.railway.app',
+        'PORT': '6060',
     }
 }
 
