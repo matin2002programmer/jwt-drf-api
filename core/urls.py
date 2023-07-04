39161747300a5ef9b10f2f343ae8cb25cf18097e
Matin <81927498+matin2002programmer@users.noreply.github.com>
2023-07-04 22:52:00 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include("accountModule.urls", namespace='accountModule')),
-    path('products/', include("product_module.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('products/', include("product_module.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
+] 
