38c6e9ab61bd144790e50933576a40688a0faf31
Matin <81927498+matin2002programmer@users.noreply.github.com>
2023-07-05 13:07:31 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.views.static import serve
 from django.conf import settings
 from django.shortcuts import redirect
@@ -10,6 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include("accountModule.urls", namespace='accountModule')),
     path('products/', include("product_module.urls")),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
