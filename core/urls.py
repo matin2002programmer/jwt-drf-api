7f7ad9078a4c26fd036bb6fb5a6cbe69b14ade45
Matin <81927498+matin2002programmer@users.noreply.github.com>
2023-07-05 19:23:10 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.static import static
+from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
@@ -6,10 +7,12 @@ from django.conf import settings
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: redirect('https://www.instagram.com/matindevilish_boy/')),
+    path('', RedirectView.as_view(url='/', permanent=True)),
+    path('/', lambda request: redirect('https://www.instagram.com/matindevilish_boy/')),
     path('admin/', admin.site.urls),
     path('api/auth/', include("accountModule.urls", namespace='accountModule')),
     path('products/', include("product_module.urls")),
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+handler404 = lambda request, exception: redirect('/')
