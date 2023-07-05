159c7b821d8ef9f88e4c8d5e3cb5068a058a671e
Matin <81927498+matin2002programmer@users.noreply.github.com>
2023-07-05 19:43:27 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls.static import static
-from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
 from django.conf import settings
 from django.shortcuts import redirect
 
+def page_not_found_view(request, exception):
+    redirect('https://www.instagram.com/matindevilish_boy/')
+
 urlpatterns = [
-    path('/', lambda request: redirect('https://www.instagram.com/matindevilish_boy/')),
+    path('', lambda request: redirect('https://www.instagram.com/matindevilish_boy/')),
     path('admin/', admin.site.urls),
     path('api/auth/', include("accountModule.urls", namespace='accountModule')),
     path('products/', include("product_module.urls")),
-] 
+]
+handler404 = "mysite.views.page_not_found_view"
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-handler404 = lambda request, exception: redirect('/')
