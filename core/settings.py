1b08cf6b3e700d7e3633f0a52715149763676863
Matin <81927498+matin2002programmer@users.noreply.github.com>
2023-07-15 13:30:16 +0330

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -203,8 +203,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
+MEDIA_URL = '/media_root/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
